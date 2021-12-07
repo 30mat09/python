65ae6d90074d13ef092799544d17c738a99b33e5
Mathieu <64844647+30mat09@users.noreply.github.com>
2021-12-07 20:10:22 +0100

    Update ABR.py

--- ABR.py
+++ ABR.py
@@ -1,24 +1,24 @@
-class ABR:
+class ABR: #Arbre Binaire de Recherche
     def __init__(self,v=None):
-        self.valeur=v
-        self.gauche=None
-        self.droite=None
+        self.data=v
+        self.left=None
+        self.right=None
     def insertData(self,x):
         if(not(isinstance(x,int)) and not(isinstance(x,float))):
             return("Must be an integer")
         else:
-            if(x>self.valeur):
-                if(self.droite is None):
-                    self.droite=ABR(x)
+            if(x>self.data):
+                if(self.right is None):
+                    self.right=ABR(x)
                 else:
-                    self.droite.insertData(x)
-            elif(x<=self.valeur):
-                if(self.gauche is None):
-                    self.gauche=ABR(x)
+                    self.right.insertData(x)
+            elif(x<=self.data):
+                if(self.left is None):
+                    self.left=ABR(x)
                 else:
-                    self.gauche.insertData(x)
+                    self.left.insertData(x)
     def estVide(self):
-        if(self.gauche is None and self.droite is None and self.valeur is None):
+        if(self.left is None and self.right is None and self.data is None):
             return(True)
         else:
             return(False)
@@ -26,49 +26,49 @@ class ABR:
         if(self.estVide()):
             return("Empty")
         else:
-            if(x==self.valeur):
+            if(x==self.data):
                 return(True)
-            elif(self.gauche is not None and x<=self.valeur):
-                return(self.gauche.search(x))
-            elif(self.droite is not None and x>self.valeur):
-                return(self.droite.search(x))
+            elif(self.left is not None and x<=self.data):
+                return(self.left.search(x))
+            elif(self.right is not None and x>self.data):
+                return(self.right.search(x))
             else:
                 return(False)
     def infixe(self):
         if(self.estVide()):
             return([])
-        elif(self.gauche is None and self.droite is None):
-            return([self.valeur])
+        elif(self.left is None and self.right is None):
+            return([self.data])
         else:
-            if(self.gauche is None):
-                return([self.valeur]+self.droite.infixe())
-            elif(self.droite is None):
-                return(self.gauche.infixe()+[self.valeur])
+            if(self.left is None):
+                return([self.data]+self.right.infixe())
+            elif(self.right is None):
+                return(self.left.infixe()+[self.data])
             else:
-                return(self.gauche.infixe()+[self.valeur]+self.droite.infixe())
+                return(self.left.infixe()+[self.data]+self.right.infixe())
     def affichage(self,decalage=""):
         if(self.estVide()):
             print(f"{decalage}-")
         else:
             print(f"{decalage}{str(self.valeur)}")
-            if(self.gauche is not None):
-                self.gauche.affichage(decalage+"   ")
+            if(self.left is not None):
+                self.left.affichage(decalage+"   ")
             else:
                 print(f"{decalage}   -")
-            if(self.droite is not None):
-                self.droite.affichage(decalage+"   ")
+            if(self.right is not None):
+                self.right.affichage(decalage+"   ")
             else:
                 print(f"{decalage}   -")
     def max(self):
-        if(self.droite is None):
-            return(self.valeur)
+        if(self.right is None):
+            return(self.data)
         else:
-            return(self.droite.max())
+            return(self.right.max())
     def min(self):
-        if(self.gauche is None):
-            return(self.valeur)
+        if(self.left is None):
+            return(self.data)
         else:
-            return(self.gauche.min())
+            return(self.left.min())
 
 def tri(liste):
     arbre=ABR(liste[0])
